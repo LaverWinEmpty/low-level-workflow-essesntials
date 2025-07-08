ba7b4e33977366b587bd7730670b817f8046fd21
KimSeungBin <77220945+LaverWinEmpty@users.noreply.github.com>
2025-07-09 06:47:12 +0900

    Update example_ptr.hpp

--- lwe/test/example_ptr.hpp
+++ lwe/test/example_ptr.hpp
@@ -1,163 +1,131 @@
 #include "../mem/ptr.hpp"
 
+#include "iostream"
+
 namespace test {
 
-#pragma region CHORE
-using namespace LWE;
-struct Dummy_ptr {
-    Dummy_ptr() = default;
-    Dummy_ptr(int in): value(in) { }
-    template<typename T> friend T& operator<<(T& os, const Dummy_ptr& in) {
-        os << in.value;
-        return os;
-    }
-    int value = 0;
-};
-using Ptr = LWE::mem::Ptr<Dummy_ptr>;
-
-template<typename T>
-void output_ptr(const mem::Ptr<T>& ptr) {
-    std::cout << ptr.get() << " ... ";
-    if(ptr) {
-        std::cout << "(" << *ptr << ")";
-    }
-    else std::cout << "NULL";
-    std::cout << std::endl; // address / reference count
-}
-#pragma endregion
-
-void example_ptr_main() {
-    /* default constructor */ {
-        Ptr ptr;
-        output_ptr(ptr);
-
-        Ptr null = nullptr; // nullptr (call `Ptr(T*, Deleter)`)
-        output_ptr(null);
-    }
-
-    /* parameter constructor*/ {
-        Ptr a = Ptr(1);
-        output_ptr(a);
-
-        Dummy_ptr value = 2;
-        Ptr       c     = value;
-        output_ptr(c);
-
-        Ptr b(3);
-        output_ptr(b);
-
-        Ptr d = new Dummy_ptr(4);
-        output_ptr(d);
-    }
-
-    /* operator= */ {
-        Ptr a = 100;
-        Ptr b = a; // copy constructor
-        *a    = 5; // weak
-        output_ptr(b);
-
-        Ptr c;
-        c  = b; // operator=
-        *a = 6; // weak
-        output_ptr(c);
-
-        a = 7;
-        a = a; // self-assignment
-        // a = a = 7; // UB
-        output_ptr(a);
-    }
-
-    /* move, get */ {
-        Ptr a;
-        Ptr b = 8;
-        a     = std::move(b);
-        output_ptr(a); // output_ptr
-        output_ptr(b); // output_ptr NULL
-
-        *a = 9;
-        printf("%p ... ", a.as<void>());  // casting
-        printf("(%d)\n", a.get()->value); // get
-
-        Dummy_ptr* get_1 = a.get();                    // get
-        Dummy_ptr* get_2 = static_cast<Dummy_ptr*>(a); // get by cast
-        void*      get_3 = a.as<void>();               // casting
-    }
-
-    /* about desturcor */ {
-        // destructor set nullptr -> memory leak
-        Dummy_ptr* heap = new Dummy_ptr;
-        { Ptr c = Ptr(heap, nullptr); }
-        delete heap; // safe
-
-        // custom deallocator
-        Ptr d = Ptr((Dummy_ptr*)malloc(sizeof(Dummy_ptr)), [](void* in) { free(in); });
-    }
-
-    /* dangling check */ {
-        Ptr weak;
-
-        /* valid case */ {
-            Ptr owner = new Dummy_ptr;
-            weak      = owner;
-
-            *owner = 10;
-            if(weak.valid()) {
-                output_ptr(weak);
-            }
-
-            *owner = 11;
-            if(weak.owned()) {
-                output_ptr(weak); // not owner
-            }
-
-            weak.own(); // set owner
-            *owner = 12;
-            if(weak.owned()) {
-                output_ptr(weak); // not owner
-            }
-
-            owner.own();
-            // free owner
-        }
-
-        /* invalid case */ {
-            // use memory pool, allow but not safe
-            // safe: if(weak) *weak = value;
-            *weak = 100;
-
-            if(weak.valid()) {
-                output_ptr(weak);
-            }
-            output_ptr(weak); // if (ptr) is false
-        }
-
-        /* clone */ {
-            Ptr a;
-            {
-                Ptr b = new Dummy_ptr;
-                a     = b; // weak
-
-                b.clone(); // b is onwer, is failed
-                a.clone(); // COW
-
-                *a = 13;
-                *b = 14;
-                if(a.owned()) {
-                    output_ptr(a);
-                }
-                if(b.owned()) {
-                    output_ptr(b);
-                }
-            }
-            *a = 15; // cloned, is safe
-            output_ptr(a);
-        }
-    }
-}
-
 void example_ptr() {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    example_ptr_main();
+    using LWE::mem::Ptr;
+
+    /* === QUICK START === */
+    Ptr<int> ptr0 = {};          // nullptr
+    Ptr<int> ptr1 = new int(42); // like `shared_ptr<int>(new int{42})`
+    Ptr<int> ptr2 = int{ 42 };   // like `std::make_shared<int>(42)`
+
+    // lambda: custom deleter, no capture
+    Ptr<int> ptr3 = { (int*)malloc(4), [](int* in) {
+                         free(in); // default: delete in;
+                     } };
+
+    ptr2 = ptr1; // weak copy
+
+    ptr3 = nullptr; // release
+
+    bool result = ptr2.own(); // transfer ownership
+
+    if(!result) { } // ptr2 is nullptr or dangling
+    else { }        // ptr2 is weak, or was the original owner.
+
+    if(ptr2.owned()) { } // check ownership
+
+    if(ptr2.valid()) { } // check dangling
+
+    if(ptr2) { } // check dangling 2
+
+    ptr1.clone(); // copy-on-write
+    ptr2.clone(); // copy-on-write, but not work: it is owner
+
+    int*  word = ptr1.get();      // get raw pointer
+    char* byte = ptr2.as<char>(); // get raw pointer by cast
+
+    int* ptr = static_cast<int*>(ptr3); // allow (explicit operator cast)
+
+    /* === DETAIL  === */ {
+        Ptr<int> weak;
+
+        /* release test*/ {
+            Ptr<int> owner = int{ 42 };
+
+            weak = owner; // store
+        }
+        // owner is released
+
+        // Ambiguity: dangling or null check
+        if(!weak) {
+            // But not really important
+            std::cout << "weak is dangling or null\n";
+        }
+        else std::cout << "weak is OK\n";
+
+        // SAFE CASE
+        if(weak) {
+            *weak = 100; // safe
+            std::cout << *weak << "\n";
+        }
+        else try {
+                *weak = 100; // THROW EXCEPTION
+            }
+            catch(...) {
+                std::cout << "weak is dangling or null\n";
+            }
+
+        Ptr<int> dangling = weak; // WARNING: DANGLING COPY
+
+        Ptr<int> owner2 = int{ 200 }; // address that are the same as the owner's block
+
+        // WARNING CASE
+        // get() is not throw exception
+        std::cout << *dangling.get() << "\n"; // 200
+        // Reason: use memory pool
+        // And, `owner` has been confirmed that it has been released normally.
+    }
+
+    /* === EDGE CASE === */ {
+        // NOTE: THREAD UNSAFE
+        // No need to do it.
+
+        // release test
+        Ptr<int> owner = int{};
+        Ptr<int> weak  = owner;
+
+        owner = nullptr; // release
+        owner = nullptr; // release again, why not?
+        weak  = nullptr; // why not?
+
+        Ptr<int> ptr1 = int();
+        // Ptr<int> ptr2 = ptr1.get(); // WARNING
+        // ptr2          = nullptr;  // DOUBLE RELEASE
+
+        // circulation test
+        struct B;
+        struct A {
+            LWE::mem::Ptr<B> b;
+        };
+        struct B {
+            LWE::mem::Ptr<A> a;
+        };
+
+        void *a, *b;
+        {
+            Ptr<A> aa = A{};
+            Ptr<B> bb = B{};
+
+            aa->b = bb;
+            bb->a = aa;
+
+            a = aa.get(); // get raw pointer
+            b = bb.get(); // get raw pointer
+        }
+        Ptr<A> aa = A{}; // use memory pool -> same address
+        Ptr<B> bb = B{}; // use memory pool -> same address
+
+        std::cout << (a == aa.get()) << " " << (b == bb.get()) << "\n"; // 1 1, same address
+        // THIS IS UNIQUE PTR, why not?
+
+        // nullptr test
+        Ptr<int> ptr = nullptr;
+        // *ptr = 100; // WARNING: CRASH
+    }
 }
 
 } // namespace test
